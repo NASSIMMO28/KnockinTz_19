789e3a2370d228c398a5304c4f490377c7695b18
NASSIMMO28 <Nassimudy19@gmail.com>
2026-04-28 10:54:29 +0300

    fix: add admin URL to CORS

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "https://knockin-frontend-71vx.vercel.app",
-    "https://knockin-admin.vercel.app" // ← your actual admin URL
+    "https://knockin-admin-hw7x.vercel.app/" // ✅ your actual admin URL
   ],
   credentials: true
 }));
